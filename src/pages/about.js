a7b4f5a76912ab79fb8a19b6fabdfc3ccc2f5150
Emy Spjuth <emy.spjuth@prisjakt.nu>
2020-11-03 19:30:18 +0100

    Adds prettier config

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,16 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO title="About" />
     <h1>Hi from the second page</h1>
     <p>Welcome to About</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
-)
+);
 
-export default SecondPage
+export default SecondPage;
